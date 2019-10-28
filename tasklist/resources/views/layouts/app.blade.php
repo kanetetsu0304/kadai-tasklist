5b47467ee20726757f9285597b8937c771dda13e
kanetetsu0304 <kanetetsu0304@gmail.com>
2019-10-28 11:16:15 +0900

    ステータスカラム追加

--- tasklist/resources/views/layouts/app.blade.php
+++ tasklist/resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
     <meta charset="utf-8">
-    <title>MessageBoard</title>
+    <title>TaskList</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
 </head>
@@ -10,7 +10,7 @@
 <body>
 <header class="mb-4">
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-        <a class="navbar-brand" href="/">MessageBoard</a>
+        <a class="navbar-brand" href="/">TaskList</a>
 
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
             <span class="navbar-toggler-icon"></span>
@@ -20,15 +20,19 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                <li class="nav-item">{!! link_to_route('tasks.create', '新規メッセージの投稿', [], ['class' => 'nav-link']) !!}</li>
+                <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクリストの投稿', [], ['class' => 'nav-link']) !!}</li>
             </ul>
         </div>
     </nav>
 </header>
 
 <div class="container">
+    @include('commons.error_tasks')
+
     @yield('content')
 </div>
+
+
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
